a3b46c098ea60fa88d9b16c1dc3426cd2421c2a5
gudbrandschistad <gschistad@dons.usfca.edu>
2018-03-26 01:17:16 -0700

    Finished event service

--- userService/src/UserService.java
+++ userService/src/UserService.java
@@ -4,34 +4,25 @@ import org.apache.velocity.app.VelocityEngine;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 
 public class UserService {
-    protected static Logger log = LogManager.getLogger();
-    private static int PORT = 8080;
+    private static int PORT = 5050;
+    private static volatile int userid = 1;
 
     public static void main(String[] args) {
 
         Server server = new Server(PORT);
-        ServletContextHandler context = new ServletContextHandler();
-
-        context.addServlet(LoginUserServlet.class,     "/login");
-        context.addServlet(LoginRegisterServlet.class, "/register");
-        context.addServlet(HotelsDisplayServlet.class,  "/viewhotels");
-        context.addServlet(AllReviewsServlet.class,  "/allreviews");
-        context.addServlet(UserReviewsServlet.class,  "/myreviews");
-        context.addServlet(AddReviewServlet.class,  "/addreview");
-        context.addServlet(EditReviewServlet.class,  "/editreview");
-        context.addServlet(HotelAttractionsServlet.class,  "/attractions");
-        context.addServlet(LoginRedirectServlet.class, "/*");
-
-
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+        UserDataMap userDataMap = new UserDataMap();
+        handler.addServletWithMapping(new ServletHolder(new RedirectServlet(userDataMap, userid)), "/*");
         System.out.println("Starting server on port " + PORT + "...");
 
         try {
             server.start();
             server.join();
-
             System.out.println("Exiting...");
         }
         catch (Exception ex) {
